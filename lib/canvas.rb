9f44d60bab9c355edc7ee544b780a86ad4c4b53d
Diptis Halder <diptis.halder@gmail.com>
2019-09-04 08:35:19 +0100

    :sparkles: added canvas_editor feature for L command - spiked

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -1,8 +1,12 @@
 class Canvas
-  attr_reader :canvas_array
+  attr_accessor :canvas_array
+  attr_reader :rows
+  attr_reader :columns
 
   def initialize(rows, columns)
     @canvas_array = Array.new(columns){Array.new(rows,'O')}
+    @rows = rows
+    @columns = columns
   end
 
 end
--- lib/canvas_editor.rb
+++ lib/canvas_editor.rb
@@ -9,7 +9,11 @@ class Command_Handler
     when 'I' then
       impl_canvas(comm_arr)
     when 'S' then
-      show_canvas 
+      show_canvas
+    when 'L' then
+      colour_pixel(comm_arr)
+    when 'C' then
+      clear_canvas
     when 'X' then "Bye!"
     else "Command not recognized, please try again."
     end
@@ -21,7 +25,7 @@ class Command_Handler
       rows = comm_arr[2].to_i
       if (1..250).include?(columns) && (1..250).include?(rows)
         @canvas = Canvas.new(columns, rows)
-        "Your #{columns} x #{rows} canvas has been created."
+        "Your new #{columns} x #{rows} canvas has been created."
       else
         "Invalid parameters to the I command, please try again."
       end
@@ -32,13 +36,38 @@ class Command_Handler
   
   def show_canvas
     if @canvas
-      @canvas.canvas_array.each { |elem|
-        puts elem.join(' ')
+      @canvas.canvas_array.each { |row|
+        puts row.join(' ')
       }
     else
       "Canvas not created yet, nothing to show."
     end
   end
+
+  def colour_pixel(comm_arr)
+    if (comm_arr.length == 4) && (comm_arr[3] =~ /[A-Z]/)
+      col = comm_arr[1].to_i
+      row = comm_arr[2].to_i
+      pixcol = comm_arr[3]
+      if (1..@canvas.columns).include?(col) && (1..@canvas.rows).include?(row)
+        @canvas.canvas_array[col - 1][row - 1] = pixcol
+        "Pixel at col = #{col} , row = #{row} now has colour #{pixcol}."
+      else
+        "Invalid coordinates for the L command, please try again."
+      end
+    else
+      "Wrong parameters to the L command, please try again."
+    end
+  end
+
+
+  def clear_canvas
+    if @canvas
+      @canvas.canvas_array.fill('O')
+    else
+      "Canvas not created yet, nothing to clear."
+    end
+  end   
 
 end
 
