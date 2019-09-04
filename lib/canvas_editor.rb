f5c9f961020e13994bc543fd679dd78cba57e0d1
Diptis Halder <diptis.halder@gmail.com>
2019-09-04 07:18:37 +0100

    :sparkles: added canvas_editor features for I and S - spiked

--- lib/canvas_editor.rb
+++ lib/canvas_editor.rb
@@ -1,30 +1,54 @@
 #!/usr/bin/env ruby
+require_relative 'canvas'
 
-def command_handler(command)
-  comm_arr = command.split(' ')
-  case (comm_arr.first) 
-  when 'I' then
+class Command_Handler
+  
+  def parse_command(command)
+    comm_arr = command.split(' ')
+    case (comm_arr.first) 
+    when 'I' then
+      impl_canvas(comm_arr)
+    when 'S' then
+      show_canvas 
+    when 'X' then "Bye!"
+    else "Command not recognized, please try again."
+    end
+  end
+
+  def impl_canvas(comm_arr)
     if comm_arr.length == 3
       columns = comm_arr[1].to_i
       rows = comm_arr[2].to_i
       if (1..250).include?(columns) && (1..250).include?(rows)
-        "Start canvas"
+        @canvas = Canvas.new(columns, rows)
+        "Your #{columns} x #{rows} canvas has been created."
       else
         "Invalid parameters to the I command, please try again."
       end
     else
       "Wrong number of parameters to the I command, please try again."
     end
-  when 'X' then "Bye!"
-  else "Command not recognized, please try again."
   end
+  
+  def show_canvas
+    if @canvas
+      @canvas.canvas_array.each { |elem|
+        puts elem.join(' ')
+      }
+    else
+      "Canvas not created yet, nothing to show."
+    end
+  end
+
 end
 
+
 puts "Welcome to the character-based canvas editor!"
+command_handler = Command_Handler.new
 loop do
   print "Enter a command (? for help): "
   command = gets.chomp
-  return_message = command_handler(command)
+  return_message = command_handler.parse_command(command)
   break if return_message == "Bye!"
   puts return_message
 end
